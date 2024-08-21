dc2f05b290925560866c94dc81d49663a0fb96cd
Hongfei Shen <hongfei.shen@dolphindb.com>
2024-08-21 16:53:52 +0800

    fix copy;

--- copy.ts
+++ copy.ts
@@ -4,6 +4,6 @@ const fpd_root = import.meta.dirname.fpd
 
 await Promise.all(
     ['vscode-oniguruma/release/onig.wasm', 'dolphindb/docs.en.json', 'dolphindb/docs.zh.json'].map(async fp => 
-        fcopy(`${fpd_root}node_modules/${fp}`, `${fpd_root}example/public/${fp}`)
+        fcopy(`${fpd_root}node_modules/${fp}`, `${fpd_root}example/public/${fp.fname}`)
     )
 )
